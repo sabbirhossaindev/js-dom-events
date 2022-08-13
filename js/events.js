402d42925911c4b6a414eace5f6cd7f7f3e4f984
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-14 04:13:02 +0600

    event all explore done

--- js/events.js
+++ js/events.js
@@ -14,8 +14,27 @@ function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
-// option: 4...
+// option: 3.... another
 const makePurpleButton = document.getElementById('make-purple');
 makePurpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
 }
+
+// option: 4 ....
+const makePinkButton = document.getElementById('make-pink');
+makePinkButton.addEventListener('click', makePin); // ('ja add korbo', function call);
+
+function makePin() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option: 4.....another
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+});
+
+// option: 4..... final ..important..
+document.getElementById('make-orange').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange';
+});
