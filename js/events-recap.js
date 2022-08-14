fc757bd729c603a04f116591dd928e2ce268bd5b
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-14 12:11:37 +0600

    events recap input text explore

--- js/events-recap.js
+++ js/events-recap.js
@@ -8,7 +8,25 @@ function handlerOnClick(){
 
  // option: 2.....
  // <button id="event-listener">Handler event by addEventListener</button>
- document.getElementById('event-listener').addEventListener('click', function(){
+ document.getElementById('event-listener').addEventListener('click', function(){ // anonimas function or callBack function
      const handlerStatus = document.getElementById('handler-status');
      handlerStatus.innerText = 'one day i am boss in javaScript..';
  });
+
+
+ // option: 2 recap... IMpotant...
+ /*
+  <p id="input-pre">Defulte input text..</p>
+    <input id="input-field" type="text">
+    <button id="btn-update">update</button>
+*/
+document.getElementById('btn-update').addEventListener('click', function () {
+    const inputField = document.getElementById('input-field');
+    const inputText = inputField.value;
+    const inputPre = document.getElementById('input-pre');
+    inputPre.innerText = inputText;
+    inputField.value = '';
+});
+
+
+// option: 2 recap.....
